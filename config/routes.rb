31298dec189137c8765dc14a2b13f9e1fed83320
Nakadamasatoshi <miyabi_0430@yahoo.co.jp>
2023-08-24 22:04:10 +0900

    frex

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #top_controller
+  get '/', to: 'top#index', as: 'index_top'
 end
