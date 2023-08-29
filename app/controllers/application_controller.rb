01034ffa6af320c5cdf157b84ef8b524343e2dce
Nakadamasatoshi <miyabi_0430@yahoo.co.jp>
2023-08-30 06:44:21 +0900

    8/29

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
-  # この行を追加
-  before_action :authenticate_user!
+  # この行を追加。でも強制的にログイン画面に移動してしまうので一旦隠す
+  # before_action :authenticate_user!
+  # これで良さそう
 
 end
